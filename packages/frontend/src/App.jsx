e5fcbd7dcfcdeea24dac8177680d56a227650ca8
sirsona <swaleh489@gmail.com>
2026-07-08 17:02:32 +0300

    feat: setup routes with authentication

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Dashboard } from "./pages/Dashboard";
+import { Login } from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/" element={<Navigate to="/dashboard" />} />
+    </Routes>
+  );
 }
 
 export default App;
